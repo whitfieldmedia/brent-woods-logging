1c445778ba3a051b2fa46ad0e6476221a3b0976f
spencerbackman <backmanspencer99@gmail.com>
2019-10-30 10:55:50 -0500

    added new logo

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './assets/icons/brent_woods_logo.png';
+import logo from './assets/icons/woods_logo.png';
 import { Link } from 'react-router-dom';
 import './assets/scss/nav.scss';
 
