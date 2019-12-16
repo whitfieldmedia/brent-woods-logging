9c4cf6beaa42fc10f10be7e6cb0de6de7eba1789
spencerbackman <backmanspencer99@gmail.com>
2019-12-16 11:23:19 -0600

    added seo

--- client/src/About.js
+++ client/src/About.js
@@ -7,7 +7,7 @@ class About extends React.Component {
             <div className="about-section">
                 <h2 className="about-header"> About Us </h2>
                 <p className="about-par">
-                    Woods Logging Sales is your go to source for buying used forestry equipment and attachments. We have a wide selection of equipment including Feller Bunchers, Skidders, Log Loaders, Log Trucks, Log Trailers, Dozers, and Attachments from Cat, John Deere, Tigercat, Prentice, Slasher, and other major brands. 
+                    Brent Woods Logging Equipment Sales is your go to forestry equipment & machinery dealer for buying machinery, equipment and attachments. We have a wide selection of equipment including Feller Bunchers, Skidders, Log Loaders, Log Trucks, Log Trailers, Dozers, and Attachments from Cat, John Deere, Tigercat, Prentice, Slasher, and other leading forestry equipment brands. 
                 </p>
                 <p className="about-par">
                     We are located in Grenada, Ms with clients across North America. When buying from us you're guaranteed to get the best equipment for your money. We try to include as much information about our equipment and attachments. If you're interested in finding out more information or want to buy equipment or attachments give the owner Brent Woods a call at (662) 229-7785. 
